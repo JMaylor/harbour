1012e8c99c3b7d4991dd6b0a1c04eb389102237e
JMaylor <60825814+JMaylor@users.noreply.github.com>
2023-09-24 17:03:21 +0100

    refactor: remove unnecessary returns from auth guard

--- middleware/auth-redirect.global.ts
+++ middleware/auth-redirect.global.ts
@@ -3,14 +3,11 @@ export default defineNuxtRouteMiddleware((to) => {
   if (!routeTo)
     return
 
-  // const locales = useNuxtApp().$i18n.locales.value
-  // console.log(useNuxtApp())
   const supabaseUser = useSupabaseUser()
 
   // Do not redirect on login route, callback route and excluded routes
   const login = 'login'
   const signup = 'signup'
-  const callback = 'confirm'
 
   // remove locale
   const baseRoute = (routeTo as string).split('___')[0]
@@ -21,20 +18,8 @@ export default defineNuxtRouteMiddleware((to) => {
       return navigateTo(useNuxtApp().$localePath('/'))
   }
 
-  if ([login, signup].includes(baseRoute) && Boolean(supabaseUser.value))
-    return navigateTo(useNuxtApp().$localePath('/'))
-
-  if ([
-    login,
-    signup,
-    callback,
-  ].includes(baseRoute))
-    return
-
-  // if (isExcluded)
-  //   return
-
-  const user = useSupabaseUser()
-  if (!user.value)
-    return navigateTo(useNuxtApp().$localePath('/login'))
+  else {
+    if (![login, signup].includes(baseRoute))
+      return navigateTo(useNuxtApp().$localePath('/login'))
+  }
 })

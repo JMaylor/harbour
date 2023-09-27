4048a82368bc1489187ec839fadaa13833e4af00
JMaylor <60825814+JMaylor@users.noreply.github.com>
2023-09-27 21:48:37 +0100

    feat: better language toggle

--- app.vue
+++ app.vue
@@ -3,24 +3,21 @@ import type { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables'
 
 const { locales, locale } = useI18n()
 const switchLocalePath = useSwitchLocalePath()
+const localeToSwitchTo = computed(() => (locales.value as LocaleObject[]).find(l => l.code !== locale.value))
 </script>
 
 <template>
-  <ul class="absolute right-0 top-0">
-    <li
-      v-for="l in (locales as LocaleObject[])"
-      :key="l.code"
-      :class="{
-        'font-bold': l.code === locale,
-      }"
+  <NuxtLink
+    v-if="localeToSwitchTo"
+    class="absolute right-1 top-0"
+    :to="switchLocalePath(localeToSwitchTo.code)"
+  >
+    <img
+      class="h-10"
+      :src="`/flags/${locale}.svg`"
+      :alt="$t('switch_language')"
     >
-      <NuxtLink
-        :to="switchLocalePath(l.code)"
-      >
-        {{ l.name }}
-      </NuxtLink>
-    </li>
-  </ul>
+  </NuxtLink>
 
   <NuxtLayout>
     <NuxtPage />

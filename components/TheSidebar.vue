609350f26fc00718e326e3513871cbc7c45417f5
JMaylor <60825814+JMaylor@users.noreply.github.com>
2023-10-06 22:50:06 +0100

    feat: sidebar

---
+++ components/TheSidebar.vue
@@ -0,0 +1,14 @@
+<script setup>
+const links = [
+  {
+    label: 'Home',
+    icon: 'i-heroicons-home',
+    to: '/',
+  },
+
+]
+</script>
+
+<template>
+  <UVerticalNavigation :links="links" />
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,15 @@
 <template>
   <TheHeader />
-  <nav class="container max-w-7xl">
-    <NuxtLink to="/areas">
-      Areas
-    </NuxtLink>
-  </nav>
   <main class="container max-w-7xl">
-    <NuxtPage />
+    <div class="grid grid-cols-12 gap-8">
+      <div class="col-span-2">
+        <aside class="sticky top-[calc(4rem+1px)] -mx-4 block max-h-[calc(100vh-4rem-1px)] overflow-y-auto px-4 py-8">
+          <TheSidebar />
+        </aside>
+      </div>
+      <div class="col-span-8 py-8">
+        <NuxtPage />
+      </div>
+    </div>
   </main>
 </template>

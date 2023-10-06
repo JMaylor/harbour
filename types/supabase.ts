976ca265346fe5670e14e2f788f07ebde53b8a7b
JMaylor <60825814+JMaylor@users.noreply.github.com>
2023-10-06 12:14:12 +0100

    chore: refresh supabase types

---
+++ types/supabase.ts
@@ -0,0 +1,92 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      area: {
+        Row: {
+          created_at: string
+          id: string
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          name: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
+      area_member: {
+        Row: {
+          area_id: string
+          is_admin: boolean
+          user_id: string
+        }
+        Insert: {
+          area_id: string
+          is_admin?: boolean
+          user_id: string
+        }
+        Update: {
+          area_id?: string
+          is_admin?: boolean
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "area_member_area_id_fkey"
+            columns: ["area_id"]
+            referencedRelation: "area"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "area_member_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      create_area_and_admin: {
+        Args: {
+          p_name: string
+        }
+        Returns: string
+      }
+      is_admin_of: {
+        Args: {
+          p_area_id: string
+        }
+        Returns: boolean
+      }
+      is_member_of: {
+        Args: {
+          p_area_id: string
+        }
+        Returns: boolean
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}

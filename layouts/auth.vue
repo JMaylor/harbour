00422cee872916e27e4f6b82b1b62b1ee036662e
JMaylor <60825814+JMaylor@users.noreply.github.com>
2023-10-07 12:42:09 +0100

    feat: dark mode and language select on auth pages

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex h-full w-full items-center justify-center">
+    <div class="absolute right-1 top-1 flex items-center gap-2">
+      <UDarkModeToggle />
+      <ULocaleSelect />
+    </div>
     <div class="max-w-sm grow rounded border p-4">
       <BoatLogo class="mx-auto my-4 h-12" />
       <NuxtPage />
